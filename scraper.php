3816281b88ad028e1fca2990f3fc116d9fc5adec
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-09 10:26:20 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,8 +7,42 @@ require 'scraperwiki/simple_html_dom.php';
 for($page = 1; $page <3; $page++)
 {
     $link = 'https://e.fbr.gov.pk/Registration/searchDetail.aspx?crup='.$page;
-    $NEWLINK    =   file_get_html($link);
-    echo $NEWLINK;
+     $cHeadres = array(
+      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language: en-US,en;q=0.5',
+      'Connection: Keep-Alive',
+      'Pragma: no-cache',
+      'Cache-Control: no-cache'
+     );
+ $MyWebsite = 'http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search';
+ function dlPage($href) {
+  global $cHeadres;
+  $ch = curl_init();
+  if($ch){
+   curl_setopt($ch, CURLOPT_URL, $href);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $cHeadres);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+   curl_setopt($ch, CURLOPT_HEADER, false);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+   $str = curl_exec($ch);
+   curl_close($ch);
+   $dom = new simple_html_dom();
+   $dom->load($str);
+   return $dom;
+  }
+ }
+echo $maincode = dlPage($link);
+    
+    
+    
+    
+   // $NEWLINK    =   file_get_html($link);
+   // echo $NEWLINK;
 }
 //
 // // Read in a page
